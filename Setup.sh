b995d37b95a553a9b628e89734dcfbe4d3f1e12b
Kenneth Kron <1031599+biofool@users.noreply.github.com>
2020-10-28 11:51:55 -0700

    Update Setup.sh
    Auto install vault

--- Setup.sh
+++ Setup.sh
@@ -1,2 +1,10 @@
+sudo apt-get install -y python3-venv
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install vault
 python3 -m pip install --user pipx
+. .profile
 pipx install poetry
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install vault
